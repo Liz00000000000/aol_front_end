b2103504b5db98a358b1aa87212e7f57bf2e61fc
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-12 18:04:57 -0400

    friendsSorted

---
+++ src/Components/IndividualFriend.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class IndividualFriend extends Component {
+    
+    findTheFriendId = (obj) => {
+        if(obj.friend_1 === this.props.loggedInUser.id){
+            return obj.friend_2
+        } else {
+            return obj.friend_1
+        }
+    }
+
+    render() {
+        const friend = this.props.users.find(user => user.id === this.findTheFriendId(this.props))
+       console.log(friend)
+        return (
+            <div>
+                {friend.isOnline ? <p><strong>{friend.username}</strong></p> : <p>{friend.username}</p>}
+            </div>
+        )
+    }
+}
+
+export default IndividualFriend
--- src/Components/InstantMessageer.js
+++ src/Components/InstantMessageer.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import IndividualFriend from './IndividualFriend'
 
 export class InstantMessageer extends Component {
     render() {
-        console.log(this.props)
         return (
             <div>
-                
+                {this.props.friends.map(friend => <IndividualFriend key={friend.id} {...friend} users={this.props.users} instantMessages={this.props.instantMesages} loggedInUser={this.props.loggedInUser} />)}
             </div>
         )
     }
