4130727fce900d69369ec12548c9a51bfd2d5f5a
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-13 12:39:03 -0400

    directions

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Email from './Email'
+import '../Styles/HomePage.css'
 // import { Link } from 'react-router-dom'
 
 export class HomePage extends Component {
@@ -14,11 +15,11 @@ export class HomePage extends Component {
     handleOnClick = () => this.setState({ seeNewMessages: !this.state.seeNewMessages, seeSentMessages: !this.state.seeSentMessages})
 
     render() {
-        if(!this.state.showMail) return <button onClick={this.seeMail}>You've Got Mail</button>
+        if(!this.state.showMail) return <button className='youve-got-mail-button' onClick={this.seeMail}>You've Got Mail</button>
         const yourMail = this.props.emails.filter(email => email.reciever_id === this.props.loggedInUser.id)
         const sentMail = this.props.emails.filter(email => email.sender_id === this.props.loggedInUser.id)
          return (
-            <div>
+            <div className='email-holder'>
                 <button onClick={this.handleOnClick}>Inbox</button>
                 <button onClick={this.handleOnClick}>OutBox</button>
                 {this.state.seeNewMessages ? yourMail.map(email => <Email users={this.props.users} key={email.id} {...email} />) : null  }
