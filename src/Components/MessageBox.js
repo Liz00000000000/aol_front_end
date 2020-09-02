dd1bde8337b198bbf795ba712e7fd6af964437d7
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-09-02 13:56:50 -0400

    MADEINSTANTMESSAGES!

--- src/Components/MessageBox.js
+++ src/Components/MessageBox.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import '../Styles/MessageBox.css'
 
 export class MessageBox extends Component{
+    state = {
+        messages: [],
+        newMessage: ''
+    }
+
+    handleOnChange = e => this.setState({ [e.target.name]: e.target.value })
+
+
+    submitMessage = () => this.setState({ messages: [...this.state.messages, this.state.newMessage], newMessage: ''  })
+
 
     render(){
         console.log(this.props)
@@ -9,7 +19,11 @@ export class MessageBox extends Component{
             <div className='instant-message'>
                 <button className='ui tiny button' onClick={this.props.removeMessageBox}>x</button>
                 {this.props.friend.first_name}
-                <input></input>
+                {this.state.messages.map((mess,index) => <p key={index}>{mess}</p>)}
+                <br></br>
+                <input onChange={this.handleOnChange} value={this.state.newMessage} name='newMessage'></input>
+                <br></br>
+                <button className='ui tiny button' onClick={this.submitMessage}>Submit</button>
             </div>
         )
     }
