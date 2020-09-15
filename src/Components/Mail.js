a94e67cc41e2b09684672197b8bb15c0e6301a0a
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-09-15 15:28:00 -0400

    yay

--- src/Components/Mail.js
+++ src/Components/Mail.js
@@ -9,16 +9,56 @@ export class Mail extends Component {
         newMessageInput: ''
     }
 
+    send = () => console.log(this.state.newMessageInput)
+
     handleOnClick = event => this.setState({ [event.target.name]: true })
 
     render() {
-        console.log(this.state)
         const thisId = this.props.history.match.params.id
         const previousEmail = thisId - 1
         const nextEmail = parseInt(thisId) + 1
         const email = this.props.emails.find(email => email.id === parseInt(thisId))
         if (!email) return <div>Loading...</div>
         const sender = this.props.users.find(user => user.id === email.sender_id)
+        if (this.state.response) return (
+            <div className='individual-email'>
+            <div className='ui large card'>
+                <Link to='/'><button onClick={this.props.keepMailOpen}>x</button></Link>
+                <img className='ui small circular image' src={sender.picture} alt={sender.first_name}/> {sender.first_name + ' ' + sender.last_name}
+                <div className='content'>
+                <br></br>Subject: {email.subject_line}
+                </div>
+                <div className='content'>
+                <div className='email-text'>
+                    <input></input>
+                </div>
+                </div>
+                <div className='extra content'>
+                    <button onClick={this.send} name='response' value='response'>Send</button>
+                    <button onClick={this.send} name='forward' value='forward'>Forward</button>
+                </div>
+                <div className='extra content'>
+                    <Link to={`/email/${previousEmail}`}>
+                    <button>
+                        <i className="left arrow icon">
+                        </i>Before</button>
+                        </Link>
+                    <Link to={`/email/${nextEmail}`}>
+                        <button>
+                        <i className="right arrow icon"></i>
+                        Next
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        )
+        // const thisId = this.props.history.match.params.id
+        // const previousEmail = thisId - 1
+        // const nextEmail = parseInt(thisId) + 1
+        // const email = this.props.emails.find(email => email.id === parseInt(thisId))
+        // if (!email) return <div>Loading...</div>
+        // const sender = this.props.users.find(user => user.id === email.sender_id)
         return (
             <div className='individual-email'>
                 <div className='ui large card'>
